43227058016dc10561cdabd2e4a78a1fc137b6ba
Reavik <reavik_gamer@hotmail.com>
2025-09-02 18:12:17 -0300

    fix link

--- index.js
+++ index.js
@@ -33,9 +33,9 @@ const twitchChannels = {
   vulkanotg:        { name: 'VulkanoTG',       color: 'bg-pink-500',   type: 'desertor' },
 };
 
-//const URL = 'robsomchatmanager.netlify.app';
+const URL = 'robsomchatmanager.netlify.app';
 //Mantenha este comentario
-const URL = 'localhost&parent=127.0.0.1';
+//const URL = 'localhost&parent=127.0.0.1';
 
 // Função auxiliar para obter classes CSS padronizadas dos botões de canal
 function getChannelButtonClasses(channelConfig) {
